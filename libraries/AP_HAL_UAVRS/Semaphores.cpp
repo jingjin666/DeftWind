1b2297f9217018e8d25d5cd4bf0a099b3c4dd027
wenyi.jing <1124427436@qq.com>
2019-05-07 20:08:44 -0700

    add semaphores interface.

--- libraries/AP_HAL_UAVRS/Semaphores.cpp
+++ libraries/AP_HAL_UAVRS/Semaphores.cpp
@@ -1,27 +1,40 @@
-
 #include "Semaphores.h"
+#include <nuttx/arch.h>
+
+extern const AP_HAL::HAL& hal;
 
 using namespace UAVRS;
 
-bool Semaphore::give() {
-    if (_taken) {
-        _taken = false;
-        return true;
-    } else {
-        return false;
-    }
+
+bool Semaphore::give() 
+{
+    return pthread_mutex_unlock(&_lock) == 0;
 }
 
-bool Semaphore::take(uint32_t timeout_ms) {
-    return take_nonblocking();
+bool Semaphore::take(uint32_t timeout_ms) 
+{
+    if (up_interrupt_context()) {
+        // don't ever wait on a semaphore in interrupt context
+        return take_nonblocking();
+    }
+    if (timeout_ms == HAL_SEMAPHORE_BLOCK_FOREVER) {
+        return pthread_mutex_lock(&_lock) == 0;
+    }
+    if (take_nonblocking()) {
+        return true;
+    }
+    uint64_t start = AP_HAL::micros64();
+    do {
+        hal.scheduler->delay_microseconds(200);
+        if (take_nonblocking()) {
+            return true;
+        }
+    } while ((AP_HAL::micros64() - start) < timeout_ms*1000);
+    return false;
 }
 
-bool Semaphore::take_nonblocking() {
-    /* No syncronisation primitives to garuntee this is correct */
-    if (!_taken) {
-        _taken = true;
-        return true;
-    } else {
-        return false;
-    }
+bool Semaphore::take_nonblocking() 
+{
+    return pthread_mutex_trylock(&_lock) == 0;
 }
+
--- libraries/AP_HAL_UAVRS/Semaphores.h
+++ libraries/AP_HAL_UAVRS/Semaphores.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "AP_HAL_UAVRS.h"
+#include <pthread.h>
+
 
 class UAVRS::Semaphore : public AP_HAL::Semaphore {
 public:
-    Semaphore() : _taken(false) {}
+    Semaphore() {
+        pthread_mutex_init(&_lock, nullptr);
+    }
     bool give();
     bool take(uint32_t timeout_ms);
     bool take_nonblocking();
 private:
-    bool _taken;
+    pthread_mutex_t _lock;
 };
