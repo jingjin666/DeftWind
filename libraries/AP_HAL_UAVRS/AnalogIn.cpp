ac2ce1281b3fce2100e8153947358f9f2b442869
wenyi.jing <1124427436@qq.com>
2019-05-08 01:38:24 -0700

    add analogin interface.

--- libraries/AP_HAL_UAVRS/AnalogIn.cpp
+++ libraries/AP_HAL_UAVRS/AnalogIn.cpp
@@ -1,52 +1,353 @@
 #include "AnalogIn.h"
+#include <drivers/drv_adc.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <nuttx/analog/adc.h>
+#include <nuttx/config.h>
+#include <arch/board/board.h>
+#include <uORB/topics/battery_status.h>
+#include <uORB/topics/servorail_status.h>
+#include <uORB/topics/system_power.h>
+#include <GCS_MAVLink/GCS_MAVLink.h>
+#include <errno.h>
+#include "GPIO.h"
+
 
 using namespace UAVRS;
 
-AnalogSource::AnalogSource(float v) :
-    _v(v)
-{}
+#define ANLOGIN_DEBUGGING 0
 
-float AnalogSource::read_average() {
-    return _v;
+// base voltage scaling for 12 bit 3.3V ADC
+#define UAVRS_VOLTAGE_SCALING (3.3f/4096.0f)
+
+#if ANLOGIN_DEBUGGING
+ # define Debug(fmt, args ...)  do {printf("%s:%d: " fmt "\n", __FUNCTION__, __LINE__, ## args); } while(0)
+#else
+ # define Debug(fmt, args ...)
+#endif
+
+extern const AP_HAL::HAL& hal;
+
+/*
+  scaling table between ADC count and actual input voltage, to account
+  for voltage dividers on the board. 
+ */
+static const struct {
+    uint8_t pin;
+    float scaling;
+} pin_scaling[] = {
+    { 4,   6.6f/4096  },    // VCC 5V rail sense
+    { 10,   3.3f/4096  },    // 3DR Brick current, usually 10.1:1
+                            // scaled from battery voltage
+    { 12,   3.3f/4096  },    // 3DR Brick voltage, usually 17:1 scaled
+};
+
+
+
+AnalogSource::AnalogSource(int16_t pin, float initial_value) :
+	_pin(pin),
+    _stop_pin(-1),
+    _settle_time_ms(0),
+    _value(initial_value),
+    _value_ratiometric(initial_value),
+    _latest_value(initial_value),
+    _sum_count(0),
+    _sum_value(0),
+    _sum_ratiometric(0)
+{
+#ifdef UAVRS_ANALOG_VCC_5V_PIN
+    if (_pin == ANALOG_INPUT_BOARD_VCC) {
+        _pin = UAVRS_ANALOG_VCC_5V_PIN;
+    }
+#endif
 }
-
-float AnalogSource::voltage_average() {
-    return 5.0f * _v / 1024.0f;
-}
-
-float AnalogSource::voltage_latest() {
-    return 5.0f * _v / 1024.0f;
-}
-
-float AnalogSource::read_latest() {
-    return _v;
-}
-
-void AnalogSource::set_pin(uint8_t p)
-{}
 
 void AnalogSource::set_stop_pin(uint8_t p)
-{}
+{ 
+    _stop_pin = p; 
+}
 
-void AnalogSource::set_settle_time(uint16_t settle_time_ms)
-{}
 
-AnalogIn::AnalogIn()
+
+float AnalogSource::read_average() 
+{
+    if (_sum_count == 0) {
+        return _value;
+    }
+    hal.scheduler->suspend_timer_procs();
+    _value = _sum_value / _sum_count;
+    _value_ratiometric = _sum_ratiometric / _sum_count;
+    _sum_value = 0;
+    _sum_ratiometric = 0;
+    _sum_count = 0;
+    hal.scheduler->resume_timer_procs();
+    return _value;
+}
+
+
+
+float AnalogSource::read_latest() 
+{
+    return _latest_value;
+}
+
+/*
+  return scaling from ADC count to Volts
+ */
+float AnalogSource::_pin_scaler(void)
+{
+    float scaling = UAVRS_VOLTAGE_SCALING;
+    uint8_t num_scalings = ARRAY_SIZE(pin_scaling) - 1;
+    for (uint8_t i=0; i<num_scalings; i++) {
+        if (pin_scaling[i].pin == _pin) {
+            scaling = pin_scaling[i].scaling;
+            break;
+        }
+    }
+    return scaling;
+}
+
+
+/*
+  return voltage in Volts
+ */
+float AnalogSource::voltage_average()
+{
+    return _pin_scaler() * read_average();
+}
+
+/*
+  return voltage in Volts, assuming a ratiometric sensor powered by
+  the 5V rail
+ */
+float AnalogSource::voltage_average_ratiometric()
+{
+    voltage_average();
+    return _pin_scaler() * _value_ratiometric;
+}
+
+/*
+  return voltage in Volts
+ */
+float AnalogSource::voltage_latest()
+{
+    return _pin_scaler() * read_latest();
+}
+
+void AnalogSource::set_pin(uint8_t pin)
+{
+    if (_pin == pin) {
+        return;
+    }
+    hal.scheduler->suspend_timer_procs();
+    _pin = pin;
+    _sum_value = 0;
+    _sum_ratiometric = 0;
+    _sum_count = 0;
+    _latest_value = 0;
+    _value = 0;
+    _value_ratiometric = 0;
+    hal.scheduler->resume_timer_procs();
+}
+
+
+
+/*
+  apply a reading in ADC counts
+ */
+void AnalogSource::_add_value(float v, float vcc5V)
+{
+    _latest_value = v;
+    _sum_value += v;
+    if (vcc5V < 3.0f) {
+        _sum_ratiometric += v;
+    } else {
+        // this compensates for changes in the 5V rail relative to the
+        // 3.3V reference used by the ADC.
+        _sum_ratiometric += v * 5.0f / vcc5V;
+    }
+    _sum_count++;
+    if (_sum_count == 254) {
+        _sum_value /= 2;
+        _sum_ratiometric /= 2;
+        _sum_count /= 2;
+    }
+}
+
+
+
+
+AnalogIn::AnalogIn() :
+    _current_stop_pin_i(0),
+	_board_voltage(0),
+    _servorail_voltage(0),
+    _power_flags(0)
 {}
 
 void AnalogIn::init()
-{}
-
-AP_HAL::AnalogSource* AnalogIn::channel(int16_t n) {
-    return new AnalogSource(1.11);
-}
-
-float AnalogIn::board_voltage(void)
 {
-    return 5.0f;
+	_adc_fd = open(ADC0_DEVICE_PATH, O_RDONLY | O_NONBLOCK);
+    if (_adc_fd == -1) {
+        AP_HAL::panic("Unable to open " ADC0_DEVICE_PATH);
+	}
+    _battery_handle   = orb_subscribe(ORB_ID(battery_status));
+    _servorail_handle = orb_subscribe(ORB_ID(servorail_status));
+    _system_power_handle = orb_subscribe(ORB_ID(system_power));
 }
 
+
+
+/*
+  move to the next stop pin
+ */
+void AnalogIn::next_stop_pin(void)
+{
+    // find the next stop pin. We start one past the current stop pin
+    // and wrap completely, so we do the right thing is there is only
+    // one stop pin
+    for (uint8_t i=1; i <= UAVRS_ANALOG_MAX_CHANNELS; i++) {
+        uint8_t idx = (_current_stop_pin_i + i) % UAVRS_ANALOG_MAX_CHANNELS;
+        UAVRS::AnalogSource *c = _channels[idx];
+        if (c && c->_stop_pin != -1) {
+            // found another stop pin
+            _stop_pin_change_time = AP_HAL::millis();
+            _current_stop_pin_i = idx;
+
+            // set that pin high
+            hal.gpio->pinMode(c->_stop_pin, 1);
+            hal.gpio->write(c->_stop_pin, 1);
+
+            // set all others low
+            for (uint8_t j=0; j<UAVRS_ANALOG_MAX_CHANNELS; j++) {
+                UAVRS::AnalogSource *c2 = _channels[j];
+                if (c2 && c2->_stop_pin != -1 && j != idx) {
+                    hal.gpio->pinMode(c2->_stop_pin, 1);
+                    hal.gpio->write(c2->_stop_pin, 0);
+                }
+            }
+            break;
+        }
+    }
+}
+
+
+
+/*
+  called at 1kHz
+ */
 void AnalogIn::_timer_tick(void)
 {
+    if (_adc_fd == -1) {
+        // not initialised yet
+        return;
+    }
+
+    // read adc at 100Hz
+    uint32_t now = AP_HAL::micros();
+    uint32_t delta_t = now - _last_run;
+    if (delta_t < 10000) {
+        return;
+    }
+    _last_run = now;
+
+    struct adc_msg_s buf_adc[UAVRS_ANALOG_MAX_CHANNELS];
+
+    // cope with initial setup of stop pin
+    if (_channels[_current_stop_pin_i] == nullptr ||
+        _channels[_current_stop_pin_i]->_stop_pin == -1) {
+        next_stop_pin();
+    }
+
+    /* read all channels available */
+    int ret = read(_adc_fd, &buf_adc, sizeof(buf_adc));
+    if (ret > 0) {
+        // match the incoming channels to the currently active pins
+        for (uint8_t i=0; i<ret/sizeof(buf_adc[0]); i++) {
+            if (buf_adc[i].am_channel == 4) {
+                // record the Vcc value for later use in
+                // voltage_average_ratiometric()
+                _board_voltage = buf_adc[i].am_data * 6.6f / 4096;
+            }
+        }
+        for (uint8_t i=0; i<ret/sizeof(buf_adc[0]); i++) {
+            Debug("chan %u value=%u\n",
+                  (unsigned)buf_adc[i].am_channel,
+                  (unsigned)buf_adc[i].am_data);
+            for (uint8_t j=0; j<UAVRS_ANALOG_MAX_CHANNELS; j++) {
+                UAVRS::AnalogSource *c = _channels[j];
+                if (c != nullptr && buf_adc[i].am_channel == c->_pin) {
+                    // add a value if either there is no stop pin, or
+                    // the stop pin has been settling for enough time
+                    if (c->_stop_pin == -1 || 
+                        (_current_stop_pin_i == j &&
+                         AP_HAL::millis() - _stop_pin_change_time > c->_settle_time_ms)) {
+                        c->_add_value(buf_adc[i].am_data, _board_voltage);
+                        if (c->_stop_pin != -1 && _current_stop_pin_i == j) {
+                            next_stop_pin();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // check for new servorail data on FMUv2
+    if (_servorail_handle != -1) {
+        struct servorail_status_s servorail;
+        bool updated = false;
+        if (orb_check(_servorail_handle, &updated) == 0 && updated) {
+            orb_copy(ORB_ID(servorail_status), _servorail_handle, &servorail);
+                if (servorail.timestamp != _servorail_timestamp) {
+                    _servorail_timestamp = servorail.timestamp;
+                    _servorail_voltage = servorail.voltage_v;
+                    for (uint8_t j=0; j<UAVRS_ANALOG_MAX_CHANNELS; j++) {
+                        UAVRS::AnalogSource *c = _channels[j];
+                        if (c == nullptr) continue;
+                        if (c->_pin == UAVRS_ANALOG_ORB_SERVO_VOLTAGE_PIN) {
+                            c->_add_value(servorail.voltage_v / UAVRS_VOLTAGE_SCALING, 0);
+                        }
+                        if (c->_pin == UAVRS_ANALOG_ORB_SERVO_VRSSI_PIN) {
+                            c->_add_value(servorail.rssi_v / UAVRS_VOLTAGE_SCALING, 0);
+                        }
+                    }
+                }
+       }
+    }
+    if (_system_power_handle != -1) {
+        struct system_power_s system_power;
+        bool updated = false;
+        if (orb_check(_system_power_handle, &updated) == 0 && updated) {
+            orb_copy(ORB_ID(system_power), _system_power_handle, &system_power);
+            uint16_t flags = 0;
+            if (system_power.usb_connected) flags |= MAV_POWER_STATUS_USB_CONNECTED;
+            if (system_power.brick_valid)   flags |= MAV_POWER_STATUS_BRICK_VALID;
+            if (system_power.servo_valid)   flags |= MAV_POWER_STATUS_SERVO_VALID;
+            if (system_power.periph_5V_OC)  flags |= MAV_POWER_STATUS_PERIPH_OVERCURRENT;
+            if (system_power.hipower_5V_OC) flags |= MAV_POWER_STATUS_PERIPH_HIPOWER_OVERCURRENT;
+            if (_power_flags != 0 && 
+                _power_flags != flags && 
+                hal.util->get_soft_armed()) {
+                // the power status has changed while armed
+                flags |= MAV_POWER_STATUS_CHANGED;
+            }
+            _power_flags = flags;
+         }
+   }
+}
+
+
+AP_HAL::AnalogSource* AnalogIn::channel(int16_t pin) 
+{
+    for (uint8_t j=0; j<UAVRS_ANALOG_MAX_CHANNELS; j++) {
+        if (_channels[j] == nullptr) {
+            _channels[j] = new AnalogSource(pin, 0.0f);
+            return _channels[j];
+        }
+    }
+    hal.console->printf("Out of analog channels\n");
+    return nullptr;
 }
 
--- libraries/AP_HAL_UAVRS/AnalogIn.h
+++ libraries/AP_HAL_UAVRS/AnalogIn.h
@@ -2,26 +2,78 @@
 
 #include "AP_HAL_UAVRS.h"
 
+#include <pthread.h>
+#include <uORB/uORB.h>
+
+
+
+#define UAVRS_ANALOG_MAX_CHANNELS 16
+
+
+#define UAVRS_ANALOG_VCC_5V_PIN                4
+#define UAVRS_ANALOG_ORB_SERVO_VOLTAGE_PIN       102
+#define UAVRS_ANALOG_ORB_SERVO_VRSSI_PIN         103
+
+
 class UAVRS::AnalogSource : public AP_HAL::AnalogSource {
 public:
-    AnalogSource(float v);
+    friend class UAVRS::AnalogIn;
+    AnalogSource(int16_t pin, float initial_value);
     float read_average();
     float read_latest();
     void set_pin(uint8_t p);
-    void set_stop_pin(uint8_t p);
-    void set_settle_time(uint16_t settle_time_ms);
     float voltage_average();
     float voltage_latest();
-    float voltage_average_ratiometric() { return voltage_average(); }
+    float voltage_average_ratiometric();
+
+    // implement stop pins
+    void set_stop_pin(uint8_t p);
+    void set_settle_time(uint16_t settle_time_ms) { _settle_time_ms = settle_time_ms; }
+
 private:
-    float _v;
+    // what pin it is attached to
+    int16_t _pin;
+    int16_t _stop_pin;
+    uint16_t _settle_time_ms;
+    
+    // what value it has
+    float _value;
+    float _value_ratiometric;
+    float _latest_value;
+    uint8_t _sum_count;
+    float _sum_value;
+    float _sum_ratiometric;
+    void _add_value(float v, float vcc5V);
+    float _pin_scaler();
 };
 
 class UAVRS::AnalogIn : public AP_HAL::AnalogIn {
 public:
     AnalogIn();
-    void init();
+    void init() override;
+    AP_HAL::AnalogSource* channel(int16_t pin) override;
     void _timer_tick(void);
-    AP_HAL::AnalogSource* channel(int16_t n);
-    float board_voltage(void);
+    float board_voltage(void) override { return _board_voltage; }
+    float servorail_voltage(void) override { return _servorail_voltage; }
+    uint16_t power_status_flags(void) override { return _power_flags; }
+
+private:
+    int _adc_fd = -1;
+    int _battery_handle;
+    int _servorail_handle;
+    int _system_power_handle;
+    uint64_t _battery_timestamp;
+    uint64_t _servorail_timestamp;
+    UAVRS::AnalogSource* _channels[UAVRS_ANALOG_MAX_CHANNELS];
+    
+    // what pin is currently held low to stop a sonar from reading
+    uint8_t _current_stop_pin_i;
+    uint32_t _stop_pin_change_time;
+    
+    uint32_t _last_run;
+    float _board_voltage;
+    float _servorail_voltage;
+    uint16_t _power_flags;
+
+    void next_stop_pin(void);
 };
