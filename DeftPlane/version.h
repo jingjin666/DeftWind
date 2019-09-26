c7cce384df32fe65c7fb8f89c838026779bc48b3
jingjin <jingjin666@gmail.com>
2019-09-26 11:11:52 +0800

    Modify Version to V2.1.1

--- DeftPlane/version.h
+++ DeftPlane/version.h
@@ -2,7 +2,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "UAVRS PLANE V2.1.0"
+#define THISFIRMWARE "UAVRS PLANE V2.1.1"
 #define FIRMWARE_VERSION 2,1,1,FIRMWARE_VERSION_TYPE_DEV
 
 #ifndef GIT_VERSION
